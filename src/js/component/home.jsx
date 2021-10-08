015d9426fb18b047cbf44a04ec46b36ff677fb9a
Javier <jmanteca@jmanvel.com>
2021-10-08 07:31:20 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Jumbo } from "./jumbo.jsx";
+import { NavBar } from "./navBar.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,19 +8,24 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid">
+			<NavBar />
+			<div className="text-center mt-5">
+				<h1>Hello Rigo!</h1>
+
+				<p>
+					<img src={rigoImage} />
+				</p>
+				<a href="#" className="btn btn-success">
+					If you see this green button... bootstrap is working
+				</a>
+				<p>
+					Made by{" "}
+					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
+					with love!
+				</p>
+				<Jumbo />
+			</div>
 		</div>
 	);
 };
